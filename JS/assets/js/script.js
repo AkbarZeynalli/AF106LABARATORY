5353120b4e7728b24e73047b4fb59637100ee812
Mirze099 <akbarhz-af106@code.edu.az>
2024-10-31 12:48:39 +0400

    js de ilk kodlarimi yazdim

---
+++ JS/assets/js/script.js
@@ -0,0 +1,162 @@
+// Sual1:
+
+// let inputNumber =Number(prompt("Eded daxil edin:"))
+// if(inputNumber % 2 >=0 ){
+//     console.log("Eded musber=tdir");
+// }
+// else{
+//     console.log("Eded menfidir");
+// }
+
+
+// Sual2:
+
+// let inputNumber =Number(prompt("Eded daxil edin:"))
+// if(inputNumber % 2 == 0 ){
+//     console.log("Eded cutdur");
+// }
+// else{
+//     console.log("Eded Tekdir");
+// }
+
+
+// Sual3:
+
+// let kilograme =Number(prompt("Cekinizi Daxil edin:"))
+// let height =Number(prompt("Boyunuzu  Daxil edin:"))
+// let bmi = kilograme /(height**2)
+// console.log(`sizin body massiniz: ${bmi}`);
+
+// Sual4:
+
+// let number = 30;
+// let inputNumber =Number(prompt("Eded daxil edin:"));
+// if (inputNumber == number) {
+//     console.log('ededi dogru texmin etdiniz');
+// }
+// else{
+// console.log("Davay isde yeniden");
+// }
+
+// Sual5:
+
+// let inputa =Number(prompt("a daxil edin:"));
+// let inputb =Number(prompt("b daxil edin:"));
+// let inputc =Number(prompt("c daxil edin:"));
+
+// if(inputa == inputb && inputa == inputc){
+//     console.log("ucbucaq beraber tereflidir");
+// }else{
+//     console.log("ucbucaq beraber yanlidir");
+// }
+
+
+
+// Sual6:
+
+// let number =Number(prompt("a daxil edin:"));
+// let start =Number(prompt("b daxil edin:"));
+// let end =Number(prompt("c daxil edin:"));
+// if(number>start && number <end){
+//     console.log("Arasdinda yerlesir ");   
+// }
+
+// Sual7:
+
+// let number1 =Number(prompt("1 cini daxil edin:"));
+// let number2 =Number(prompt("2 cini daxil edin:"));
+// let sum;
+// let operator =String(prompt("operatoru daxil edin:"));
+// if(operator == "+"){
+//     sum = number1 + number2
+//     console.log(`${sum}`);
+// }
+// else if(operator == "-"){
+//     sum = number2 - number1
+//     console.log(`${sum}`);
+// }
+// else if(operator == "*"){
+//     sum = number2 * number1
+//     console.log(`${sum}`);
+// }
+// else if(operator == "/"){
+//     sum = number2 / number1
+//     console.log(`${sum}`);
+// }
+
+// Sual8:
+
+// let inputMonth = Number(prompt("Necenci ay lazimdir?"));
+
+// switch (key) {
+//     case 1:
+//         console.log("Yanvar ayinda 31 gun var ");
+
+//         break;
+//     case 2:
+//         console.log("Fevral ayinda 28 ve 29  gun var ");
+//         break;
+//     case 3:
+//         console.log("Mart ayinda 31 gun var ");
+//         break;
+//     case 4:
+//         console.log("Aprel ayinda 30 gun var ");
+//         break;
+//     case 5:
+//         console.log("May ayinda 31 gun var ");
+//         break;
+//     case 6:
+//         console.log("iyun ayinda 30 gun var ");
+//         break;
+//     case 7:
+//         console.log("iyul ayinda 31 gun var ");
+//         break;
+//     case 8:
+//         console.log("AVGUST ayinda 31 gun var ");
+//         break;
+//     case 9:
+//         console.log("Sentiyabr ayinda 30 gun var ");
+//         break;
+//     case 10:
+//         console.log("Oktyabr ayinda 31 gun var ");
+//         break;
+//     case 11:
+//         console.log("Noyabr ayinda 30 gun var ");
+//         break;
+//     case 12:
+//         console.log("Dekabr ayinda 31 gun var ");
+//         break;
+
+// }
+
+
+// Sual9:
+// let a=Number(prompt("ilk eded"))
+// let b=Number(prompt('ikinci eded'))
+// let c=Number(prompt('ucuncu eded'))
+
+// if(a>b && b>c){
+//     console.log(a+c);
+    
+// }
+// else if(a>c && c>b){
+//     console.log(a+b);
+    
+// }
+// else if(b>a && b>c){
+//     console.log(b+c);
+    
+// }
+// else if(b>c && c>a){
+//     console.log(b+a);
+    
+// }
+// else if(c>a && a>b){
+//     console.log(c+b);
+    
+// }
+
+// else{
+//     console.log(c+b);
+    
+// }
