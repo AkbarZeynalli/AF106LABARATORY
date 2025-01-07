6cb29fca6da4df8d91a2b34dafb502044b57335a
feridshahmur <akbarhz-af106@code.edu.az>
2025-01-07 13:27:35 +0400

    added last

--- NODE/backend/index.js
+++ NODE/backend/index.js
@@ -6,18 +6,34 @@ app.use(express.json())
 const port = 3000
 let items = []
 
-app.get("/items",(req,res)=>{
+app.get("/items", (req, res) => {
     res.json(items)
 })
 
-app.post("/items",(req,res)=>{
-    const {id,name} = req.body
-    const newItem = {id,name}
+app.post("/items", (req, res) => {
+    const { id, name } = req.body
+    const newItem = { id, name }
     items.push(newItem)
     res.status(200).json(items)
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("Server is runing");
-    
+
+})
+
+app.put("/items/:id", (req, res) => {
+    const id = parseInt(req.params.id, 10)
+    const { name } = req.body
+    if (!id) {
+        res.status(404).json("id not find")
+    }
+
+    if (!name) {
+        res.status(404).json("name not find")
+    }
+
+    const item = items.find(item => item.id === id)
+    item.name = name
+    res.json(item)
 })
